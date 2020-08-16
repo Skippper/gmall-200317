d348cda55c81e64fa38cd2dfde296247880ed058
Skipper <Skippper@163.com>
2020-08-16 20:27:26 +0800

    跨批次处理与批次处理,写入Hbase

--- gmall-realTime/src/main/scala/com/atguigu/app/DauApp.scala
+++ gmall-realTime/src/main/scala/com/atguigu/app/DauApp.scala
@@ -7,11 +7,12 @@ import com.alibaba.fastjson.JSON
 import com.atguigu.bean.StartUpLog
 import com.atguigu.handler.DauHandler
 import com.atguigu.utils.MyKafkaUtil
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-
+import org.apache.phoenix.spark._
 /**
  * @author Skipper
  * @date 2020/08/15 
@@ -41,18 +42,28 @@ object DauApp {
       startUpLog
     })
 
-    startLogDStream.cache()
-    println("去重前有" +  startLogDStream.count().print() + "条")
+//    startLogDStream.cache()
+//    startLogDStream.count().print()
     //5.根据Redis中保存的数据进行跨批次去重
-    val  filterStartLogDStrean : DStream[StartUpLog] = DauHandler.filterRedisLog(startLogDStream)
-    filterStartLogDStrean.cache()
-    println("去重后有" +  filterStartLogDStrean.count().print() + "条")
+    //当第二次去重后的结果写入redis时 数据才会变,此时的数据是不变的 所以可以使用广播变量的形式将数据播送出去
+    //广播变量不要超过200M
+    val  filterStartLogDStrean : DStream[StartUpLog] = DauHandler.filterRedisLog(startLogDStream,ssc.sparkContext)
+//    filterStartLogDStrean.cache()
+//    filterStartLogDStrean.count()
     //6.对第一次去重后的数据做同批次去重
 
+    val  filterOnSameBatchDStream : DStream[StartUpLog] = DauHandler.filterOnSameBatch(filterStartLogDStrean)
+    filterOnSameBatchDStream.cache()
+    //filterOnSameBatchDStream.count().print()
     //7.将两次去重后的数据(mid)写入Redis
-    DauHandler.saveMidToRedis(filterStartLogDStrean)
+    DauHandler.saveMidToRedis(filterOnSameBatchDStream)
     //8.将数据保存至HBase(Phoenix)
-
+    filterOnSameBatchDStream.foreachRDD(rdd => {
+      rdd.saveToPhoenix("GMALL200317_DAU",
+      classOf[StartUpLog].getDeclaredFields.map(_.getName.toUpperCase()),
+      HBaseConfiguration.create(),
+      Some("hadoop102,hadoop103,hadoop104:2181"))
+    })
     //9.启动任务
     ssc.start()
     ssc.awaitTermination()
