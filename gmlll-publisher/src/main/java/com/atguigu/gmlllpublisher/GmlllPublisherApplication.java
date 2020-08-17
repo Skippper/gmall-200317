bf7f5a90adba276fea4480ed85f58a36b7b36d37
Skipper <Skippper@163.com>
2020-08-17 13:45:18 +0800

    初始项目提交,serivice层初始提交

---
+++ gmlll-publisher/src/main/java/com/atguigu/gmlllpublisher/GmlllPublisherApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmlllpublisher;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan(basePackages = "com.atguigu.gmlllpublisher.mapper")
+public class GmlllPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmlllPublisherApplication.class, args);
+    }
+
+}
