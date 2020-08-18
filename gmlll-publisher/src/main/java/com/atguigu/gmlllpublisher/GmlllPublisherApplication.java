86cdf7676eb117af655e5aa74e88e911dec59c5b
Skipper <Skippper@163.com>
2020-08-18 15:51:38 +0800

    添加新配置 依赖等

--- gmlll-publisher/src/main/java/com/atguigu/gmlllpublisher/GmlllPublisherApplication.java
+++ gmlll-publisher/src/main/java/com/atguigu/gmlllpublisher/GmlllPublisherApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan(basePackages = "com.atguigu.gmlllpublisher.mapper")
+@MapperScan(basePackages = "com.atguigu.gmlllpublisher.mapper")
 public class GmlllPublisherApplication {
 
     public static void main(String[] args) {
